1e351354325158ab67ee623292a1aa922df18a72
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-12-12 12:01:27 +0200

    added migration file sql.

---
+++ migration_files/V1__create_core_routing_system_tables.sql
@@ -0,0 +1,143 @@
+do $$
+    begin
+        if not exists (select 1 from pg_type where typname = 'type_vehicle') then
+            create type type_vehicle as enum ('Truck', 'Semi-trailer', 'Cargo-vans');
+        end if;
+    end $$;
+
+do $$
+    begin
+        if not exists (select 1 from pg_type where typname = 'status_type') then
+            create type status_type as enum ('On the way', 'Completed', 'Stopping', 'Failed');
+        end if;
+    end $$;
+create table if not exists drivers
+(
+    id         uuid primary key,
+    first_name varchar(50)                                    not null,
+    last_name  varchar(50)                                    not null,
+    phone      varchar(15) check (phone ~ '^\+?[0-9]{7,15}$') not null unique,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp
+);
+
+create table if not exists vehicles
+(
+    id         uuid primary key,
+    brand      varchar(50)  not null,
+    type       type_vehicle not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp
+);
+
+create table if not exists cargoes
+(
+    id         uuid primary key,
+    name       varchar(50)                         not null,
+    weight     double precision check (weight > 0) not null,
+    sender     varchar(100)                        not null,
+    receipt    varchar(100)                        not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp
+);
+
+create table if not exists routes
+(
+    id          uuid primary key,
+    driver_id   uuid         not null references drivers (id) on delete set null on update cascade,
+    vehicles_id uuid         not null references vehicles (id) on delete set null on update cascade,
+    cargoes_id  uuid         not null references cargoes (id) on delete set null on update cascade,
+    start_point varchar(150) not null,
+    end_point   varchar(150) not null,
+    status status_type not null,
+    created_at  timestamp default current_timestamp,
+    updated_at  timestamp default current_timestamp
+);
+
+do $$
+    begin
+        if not exists (
+            select 1
+            from pg_indexes
+            where tablename = 'routes' and indexname = 'idx_routes_driver_id'
+        ) then
+            create index idx_routes_driver_id on routes (driver_id);
+        end if;
+
+        if not exists (
+            select 1
+            from pg_indexes
+            where tablename = 'routes' and indexname = 'idx_routes_vehicles_id'
+        ) then
+            create index idx_routes_vehicles_id on routes (vehicles_id);
+        end if;
+
+        if not exists (
+            select 1
+            from pg_indexes
+            where tablename = 'routes' and indexname = 'idx_routes_cargoes_id'
+        ) then
+            create index idx_routes_cargoes_id on routes (cargoes_id);
+        end if;
+    end $$;
+
+create or replace function update_timestamp()
+    returns trigger as
+$$
+begin
+    new.updated_at = current_timestamp;
+    return new;
+end;
+$$ language plpgsql;
+
+do $$
+    begin
+        if not exists (
+            select 1
+            from pg_trigger
+            where tgname = 'trg_update_timestamp_drivers'
+        ) then
+            create trigger trg_update_timestamp_drivers
+                before update
+                on drivers
+                for each row
+            execute function update_timestamp();
+        end if;
+
+        if not exists (
+            select 1
+            from pg_trigger
+            where tgname = 'trg_update_timestamp_vehicles'
+        ) then
+            create trigger trg_update_timestamp_vehicles
+                before update
+                on vehicles
+                for each row
+            execute function update_timestamp();
+        end if;
+
+        if not exists (
+            select 1
+            from pg_trigger
+            where tgname = 'trg_update_timestamp_cargoes'
+        ) then
+            create trigger trg_update_timestamp_cargoes
+                before update
+                on cargoes
+                for each row
+            execute function update_timestamp();
+        end if;
+
+        if not exists (
+            select 1
+            from pg_trigger
+            where tgname = 'trg_update_timestamp_routes'
+        ) then
+            create trigger trg_update_timestamp_routes
+                before update
+                on routes
+                for each row
+            execute function update_timestamp();
+        end if;
+    end $$;
+
