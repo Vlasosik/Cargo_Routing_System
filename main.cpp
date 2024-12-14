54355dbc5172c237e108319ae8a95bb251979ecd
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-12-14 22:10:36 +0200

    added to main class HttpServer.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <migration/MigrationSQL.h>
 
+#include "http_server/HttpServer.h"
+/*
+ *   migration example code
+ *   MigrationSQL migration_sql;
+ *   migration_sql.executeMigrations("/Users/vlasik/Навчання/С++/code/intelligent_cargo_routing_system/migration_files");
+ */
 int main() {
-    MigrationSQL migration_sql;
-    migration_sql.executeMigrations("/Users/vlasik/Навчання/С++/code/intelligent_cargo_routing_system/migration_files");
+    HttpServer server(8080);
+    server.start();
     return 0;
 }
