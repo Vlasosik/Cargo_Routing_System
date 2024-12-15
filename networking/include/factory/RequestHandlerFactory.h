f34a456a66d63dcf040ed44a29324472a02bd033
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-12-15 21:50:11 +0200

    temporary request validation added

--- networking/include/factory/RequestHandlerFactory.h
+++ networking/include/factory/RequestHandlerFactory.h
@@ -5,6 +5,5 @@
 class RequestHandlerFactory : public  Poco::Net::HTTPRequestHandlerFactory{
 public:
     Poco::Net::HTTPRequestHandler * createRequestHandler(const Poco::Net::HTTPServerRequest &request) override;
-
 };
 #endif //REQUESTHANDLERFACTORY_H
